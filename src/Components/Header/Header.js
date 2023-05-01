0cbfb0e5bb4d00361e726c3a5df71ecda43a608e
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-01 15:47:52 -0500

    add components

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+
+    render() {
+
+        return (
+            <>
+                <h1>Welcome to Can of Books</h1>
+            </>
+        )
+    }
+
+
+
+
+
+}
+
+
+export default Header;
