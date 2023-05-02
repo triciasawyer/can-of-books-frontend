7d9a985b96d551c6235d1ce70212748e05b871a2
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-02 17:13:48 -0500

    create route to about me page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import '../../App.css';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
-
 
     render() {
 
         return (
             <>
                 <h1>Can of Books</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to={"/"}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={"about"}>About Us</Link>
+                        </li>
+                    </ul>
+                </nav>
             </>
-        )
+        );
     }
-
-
-
 
 
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import About from './Components/About';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+    <Route path="/about" element={<About/>} />
+    </Route>
+  )
+);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* {<App>} */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
