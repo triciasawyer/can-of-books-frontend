eb92c409d2a1b26e4ee56353e3f83f4ef0c2e871
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-02 15:41:24 -0500

    edit styling and get carousel to work

--- src/Components/BestBooks/BestBooks.js
+++ src/Components/BestBooks/BestBooks.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
+import bookImg from '../../book.png';
+import '../../App.css';
+
 
 class BestBooks extends React.Component {
     constructor(props) {
@@ -32,34 +35,35 @@ class BestBooks extends React.Component {
 
     render() {
         console.log(this.state.books);
+        // return console.log('Yerrrrr', book.title);
 
-        let allBooks = this.state.books.map((book, index) => {
-            // return console.log('Yerrrrr', book.title);
-            return (
-            // <Carousel.Item key={index}>
-
-            //     <Carousel.Caption>
-                    <h3>Title: {book.title} </h3>
-            //     </Carousel.Caption>
-            // </Carousel.Item>
-            );
-        });
-
-
-// console.log('ALLLLLLL', allBooks);
         return (
             <>
-                {
-                    this.state.books.length > 0 && 
-
-                <Carousel>
-                  {allBooks}
-                </Carousel>
-                }
+                {this.state.books.length ? (
+                    <Carousel id='carousel'>
+                        {this.state.books.map(book => (
+                            <Carousel.Item key={book._id}>
+                                <img className='image'
+                                    src={bookImg}
+                                    alt={book.title} 
+                                    />
+                                <Carousel.Caption>
+                                    <h3>Title: {book.title} </h3>
+                                    <p>Description: {book.description}</p>
+                                    <p>Status: {book.status}</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                )   
+                       : <h3 className='no-books-found'> No Books Found</h3> }                
             </>
+
         )
+                        
     }
 }
+
 
 
 
