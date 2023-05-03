7e270fc0dd662fdb456f5582d167f48a95bb9eed
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-03 14:10:53 -0500

    edit import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './Components/Header/Header.js';
 import Main from './Components/Main/Main.js';
 import Footer from './Components/Footer/Footer.js';
-import About from './Components/About.js';
 
 
 
